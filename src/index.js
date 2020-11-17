efdaefad6f7751d270ea5fd3472831af1f9ba3b8
dannythadon <71056488+dannythadon@users.noreply.github.com>
2020-11-17 01:52:04 -0500

    Added routing to basic news and about pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./index.css";
+
 import App from "./App";
+import News from "./components/Pages/News";
+import About from "./components/Pages/About";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+  <div>
+      <Switch>
+        <Route path="/" component={App} exact/>
+        <Route path="/news" component={News}/>
+        <Route path="/about" component={About}/>
+      </Switch>
+  </div>
   </BrowserRouter>,
   document.getElementById("root")
 );
